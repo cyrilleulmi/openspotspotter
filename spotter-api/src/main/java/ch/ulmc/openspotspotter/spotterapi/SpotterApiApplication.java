dff61dec8e91ae75c8e0bd95f1c5cc626e7d8643
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2023-04-01 13:28:34 +0200

    initial commit

---
+++ spotter-api/src/main/java/ch/ulmc/openspotspotter/spotterapi/SpotterApiApplication.java
@@ -0,0 +1,13 @@
+package ch.ulmc.openspotspotter.spotterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotterApiApplication.class, args);
+	}
+
+}
---
+++ spotter-api/src/test/java/ch/ulmc/openspotspotter/spotterapi/SpotterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.ulmc.openspotspotter.spotterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
