e84a05b6e8edb506ba05ee96a07b62d82292b83c
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2023-04-09 14:12:24 +0200

    Now sends simple response to telegram updates

--- spotter-api/src/main/java/ch/ulmc/openspotspotter/spotterapi/telegram/Bot.java
+++ spotter-api/src/main/java/ch/ulmc/openspotspotter/spotterapi/telegram/Bot.java
@@ -3,7 +3,9 @@ package ch.ulmc.openspotspotter.spotterapi.telegram;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class Bot extends TelegramLongPollingBot {
@@ -15,7 +17,13 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        System.out.println(update);
+        SendMessage sendMessage = SendMessage.builder().chatId(update.getMessage().getChatId().toString())
+                .text("Hello " + update.getMessage().getFrom().getFirstName()).build();
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        };
     }
 
     @Override
