36e7dfa2f34d5765c71495b7616dff4aa2aae6f2
Cyrille Ulmi <cyrille.ulmi@bluewin.ch>
2023-04-09 14:14:42 +0200

    Now closes the bot session, should prevent error
    org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException: Error getting updates: [409] Conflict: terminated by other getUpdates request; make sure that only one bot instance is running
    
    Spring auto reload is slowed massively by this change. But as of now, this is irrelevant.

--- spotter-api/src/main/java/ch/ulmc/openspotspotter/spotterapi/telegram/Services/BotService.java
+++ spotter-api/src/main/java/ch/ulmc/openspotspotter/spotterapi/telegram/Services/BotService.java
@@ -3,9 +3,11 @@ package ch.ulmc.openspotspotter.spotterapi.telegram.Services;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.BotSession;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import ch.ulmc.openspotspotter.spotterapi.telegram.Bot;
+import jakarta.annotation.PreDestroy;
 import jakarta.inject.Inject;
 
 @Service
@@ -13,9 +15,16 @@ public class BotService {
     @Inject
     private Bot bot;
 
+    BotSession session;
+
     public BotService(Bot bot) throws TelegramApiException {
         this.bot = bot;
         TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        botsApi.registerBot(this.bot);
+        this.session = botsApi.registerBot(this.bot);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        this.session.stop();
     }
 }
